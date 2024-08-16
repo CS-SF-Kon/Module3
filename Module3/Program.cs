a2a1267f1b965fab8188e05b1492873722a7228d
CS-SF-Kon <vladislav.konovalov@evraz.com>
2024-08-16 22:19:18 +0700

    Module3: experiments with different types

--- Module3/Program.cs
+++ Module3/Program.cs
@@ -4,10 +4,20 @@
     {
         static void Main(string[] args)
         {
-            String myName;
+            string myName;
+            byte myAge;
+            bool pet;
+            float shoeSize;
             myName = "Slavik";
+            myAge = 30;
+            pet = true;
+            shoeSize = 42.5F;
 
-            Console.WriteLine(myName);
+            
+            Console.WriteLine($"My name is {myName}");
+            Console.WriteLine($"My age is {myAge}");
+            Console.WriteLine($"Do i have a pet? {pet}");
+            Console.WriteLine($"My shoe size is {shoeSize}");
 
             Console.ReadKey();
         }
